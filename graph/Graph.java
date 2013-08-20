81dab9f4b32cd8b8c45eef8a1e8ef20941e67b7b
zian <zian@132.248.36.221>
2013-08-20 18:50:52 -0500

    update graph constructor

--- graph/Graph.java
+++ graph/Graph.java
@@ -11,8 +11,9 @@ import java.util.Iterator;
 import java.util.Set;
 
 /**
- * An ady 
-* @author <a ref ="zianfanti@gmail.com"> Zian Fanti<a/>
+ * An ady
+ *
+ * @author <a ref ="zianfanti@gmail.com"> Zian Fanti<a/>
  * @version 0.0.1-0
  */
 public class Graph {
@@ -23,51 +24,63 @@ public class Graph {
     private HashMap<Vertex, ArrayList<Edge>> graph;
 
     /**
-     *
+     * Default constructor
      */
     public Graph() {
         this.graph = new HashMap<Vertex, ArrayList<Edge>>();
     }
 
     /**
-     * 
-     * @param numOfVertexex 
+     * creates a graph with a specified number of vertexes
+     *
+     * @param numOfVertexex total amout of vertex
      */
     public Graph(int numOfVertexex) {
         this.graph = new HashMap<Vertex, ArrayList<Edge>>(numOfVertexex);
     }
-    
+
     /**
+     * Create a graph with some vertxes and edges. The vertexes of the edges must 
+     * be contained in the vertexes set.
      *
-     * @param vertexes
-     * @param edges
+     * @param vertexes an array of vertex
+     * @param edges an array of edges
      */
-    public Graph(Vertex[] vertexes, Edge[][] edges) {
-        this.graph = new HashMap<Vertex, ArrayList<Edge>> (vertexes.length);
+    public Graph(Vertex[] vertexes, Edge[] edges) {
+        this.graph = new HashMap<Vertex, ArrayList<Edge>>(vertexes.length);
+        
         for (int i = 0; i < vertexes.length; i++) {
-            ArrayList<Edge> vertexEdges = new ArrayList<Edge>();                        
-            vertexEdges.addAll(Arrays.asList(edges[i]));
+            ArrayList<Edge> vertexEdges = new ArrayList<Edge>();
             graph.put(vertexes[i], vertexEdges);
         }
+        
+        for (int i = 0; i < edges.length; i++) {
+            Edge edge = edges[i];
+            Vertex source = edge.getSource();
+            ArrayList<Edge> tempEdges = graph.get((Vertex) source);
+            tempEdges.add(edge);
+            graph.put(source, tempEdges);
+        }
+
     }
-    
+
     /**
-     * 
+     *
      * @param vertex
-     * @param edges 
+     * @param edges
      */
     public void addConnectedVertex(Vertex vertex, ArrayList<Edge> edges) {
         graph.put(vertex, edges);
     }
-    
+
     /**
-     * 
+     *
      */
     public void addVertex(Vertex vertex) {
-        ArrayList<Edge> edges = new ArrayList<Edge>();        
+        ArrayList<Edge> edges = new ArrayList<Edge>();
         graph.put(vertex, edges);
     }
-    
+
     /**
      *
      * @param edge
@@ -78,7 +91,7 @@ public class Graph {
         edges.add(edge);
         graph.put(source, edges);
     }
-    
+
     /**
      *
      * @return
@@ -86,11 +99,10 @@ public class Graph {
     public int numOfVertexes() {
         return graph.size();
     }
-   
-    
+
     /**
-     * Build a string representation of the adjency matrix, corresponding of this
-     * graph.
+     * Build a string representation of the adjency matrix, corresponding of
+     * this graph.
      *
      * @return a string
      */
@@ -99,20 +111,20 @@ public class Graph {
         String graphString = "";
         Set vertexes = graph.keySet();
         Iterator vertexIterator = vertexes.iterator();
-        
-        while(vertexIterator.hasNext()) {
-            Vertex v = (Vertex) vertexIterator.next();            
+
+        while (vertexIterator.hasNext()) {
+            Vertex v = (Vertex) vertexIterator.next();
             graphString += (v.getName() + " -> [");
             ArrayList<Edge> edges = graph.get((Vertex) v);
-            for (int i = 0; i < edges.size(); i++) {                
+            for (int i = 0; i < edges.size(); i++) {
                 graphString += (i == 0) ? "" : ", ";
                 Edge e = edges.get(i);
-                graphString += ("(" + e.getTarget().getName() + ", " +
-                        e.getWeight() + ")");
+                graphString += ("(" + e.getTarget().getName() + ", "
+                        + e.getWeight() + ")");
             }
             graphString += ("] \n");
         }
-        
+
         return graphString;
     }
 }
